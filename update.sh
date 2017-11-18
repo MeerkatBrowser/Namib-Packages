c3c5555ac84a6fa68a33cddb22276865d9227f2e
frederic2ec <freddy.cho@hotmail.com>
2017-11-17 20:35:57 -0500

    Update

--- update.sh
+++ update.sh
@@ -23,6 +23,7 @@ function cloneall() {
     git clone https://github.com/MeerkatBrowser/namib-keyring
     git clone https://github.com/MeerkatBrowser/namib-mirrorlist
     git clone https://github.com/MeerkatBrowser/namib-settings-manager
+    git clone https://github.com/MeerkatBrowser/nhwd
     git clone https://aur.archlinux.org/yaourt.git
     git clone https://aur.archlinux.org/pamac-aur.git
     git clone https://aur.archlinux.org/mkinitcpio-openswap.git
