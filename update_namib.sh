845429b581a3cc600c5243e232a094db83faf33e
frederic2ec <freddy.cho@hotmail.com>
2018-06-25 20:20:41 -0400

    Update

--- update_namib.sh
+++ update_namib.sh
@@ -29,21 +29,6 @@ function cloneall() {
     git clone https://github.com/namiblinux/materia-namib-theme
     git clone https://github.com/namiblinux/materia-namib-kde
     git clone https://github.com/namiblinux/gnome-shell-extension-arc-menu-git
-    git clone https://aur.archlinux.org/trizen.git
-    git clone https://aur.archlinux.org/pamac-aur.git
-    git clone https://aur.archlinux.org/mkinitcpio-openswap.git
-    git clone https://aur.archlinux.org/package-query.git
-    git clone https://aur.archlinux.org/rhythmbox-plugin-tray-icon-git.git
-    git clone https://aur.archlinux.org/brisk-menu.git
-    git clone https://aur.archlinux.org/numix-kde-theme-git.git
-    git clone https://aur.archlinux.org/numix-circle-icon-theme-git.git
-    git clone https://aur.archlinux.org/numix-icon-theme-git.git
-    git clone https://aur.archlinux.org/pamac-tray-appindicator.git
-    git clone https://aur.archlinux.org/b43-firmware.git
-    git clone https://aur.archlinux.org/timeshift.git
-    git clone https://aur.archlinux.org/xcursor-breeze.git
-    git clone https://aur.archlinux.org/gnome-shell-extension-dash-to-panel.git
-
 
     #Get calamares package
     get_package "calamares" \
