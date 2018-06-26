bf9f7773efc89bb84e81ebb6a51aad2b5b0cf2b6
frederic2ec <freddy.cho@hotmail.com>
2018-06-25 20:19:41 -0400

    Update

---
+++ build_namibtesting.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# This file is part of the Namib GNU/Linux distribution
+# Copyright (c) 2017 Meerkat Software
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+function copyto_upload_dir() {
+    mv *.pkg.tar.xz ../upload
+}
+
+function make_loop() {
+    mkdir upload
+    for dir in */ ;
+    do
+        dir=${dir%*/}
+        if [ "$dir" == "." ] || [ "$dir" == ".." ] || [ "$dir" == "upload" ]; then
+            continue;
+        fi
+	    cd $dir
+	    makepkg -f -s -c -d
+        copyto_upload_dir
+        echo "makepkg from "$dir" finished"
+        cd ..
+    done
+}
+
+function sign_packages(){
+    cd upload
+    FILES=*.pkg.tar.xz
+    for f in $FILES
+    do
+        echo "Signing $f file..."
+        gpg --detach-sign --no-armor $f
+    done
+
+    cd ..
+}
+
+function create_repo() {
+    repo-add --sign upload/namibtesting.db.tar.gz upload/*.pkg.tar.xz
+}
+
+make_loop
+sign_packages
+create_repo
---
+++ update_aur.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# This file is part of the Namib GNU/Linux distribution
+# Copyright (c) 2017 Meerkat Software
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+function cleanup() {
+    rm -Rrf -- */
+}
+
+function cloneall() {
+    git clone https://aur.archlinux.org/trizen.git
+    git clone https://aur.archlinux.org/pamac-aur.git
+    git clone https://aur.archlinux.org/mkinitcpio-openswap.git
+    git clone https://aur.archlinux.org/package-query.git
+    git clone https://aur.archlinux.org/rhythmbox-plugin-tray-icon-git.git
+    git clone https://aur.archlinux.org/brisk-menu.git
+    git clone https://aur.archlinux.org/numix-kde-theme-git.git
+    git clone https://aur.archlinux.org/numix-circle-icon-theme-git.git
+    git clone https://aur.archlinux.org/numix-icon-theme-git.git
+    git clone https://aur.archlinux.org/pamac-tray-appindicator.git
+    git clone https://aur.archlinux.org/b43-firmware.git
+    git clone https://aur.archlinux.org/timeshift.git
+    git clone https://aur.archlinux.org/xcursor-breeze.git
+    git clone https://aur.archlinux.org/gnome-shell-extension-dash-to-panel.git
+    git clone https://aur.archlinux.org/pix.git
+    git clone https://aur.archlinux.org/xed.git
+    git clone https://aur.archlinux.org/xviewer.git
+    git clone https://aur.archlinux.org/xviewer-plugins.git
+    git clone https://aur.archlinux.org/xplayer.git
+    git clone https://aur.archlinux.org/vivaldi.git
+    git clone https://aur.archlinux.org/whalebird-bin.git
+    git clone https://aur.archlinux.org/skypeforlinux-stable-bin.git
+    git clone https://aur.archlinux.org/spotify.git
+    git clone https://aur.archlinux.org/lutris.git
+    git clone https://aur.archlinux.org/gyazo.git
+    git clone https://aur.archlinux.org/google-chrome.git
+    git clone https://aur.archlinux.org/dropbox.git
+    git clone https://aur.archlinux.org/bitwarden.git
+    git clone https://aur.archlinux.org/visual-studio-code-bin.git
+    git clone https://aur.archlinux.org/discord.git
+    git clone https://aur.archlinux.org/android-studio.git
+    git clone https://aur.archlinux.org/slack-desktop.git
+    git clone https://aur.archlinux.org/rambox.git
+}
+
+function get_package() {
+    package=$1
+    pkbuild_url=$2
+    install_url=$3
+    mkdir $package
+    cd $package
+    wget $pkbuild_url
+    if [ -n "$install_url" ]; then
+        wget $install_url
+    fi
+    cd ..
+}
+
+function delete_dotgit_dirs() {
+    for dir in */ ;
+    do
+        dir=${dir%*/}
+        if [ "$dir" == "." ] || [ "$dir" == ".." ]; then
+            continue;
+        fi
+	    cd $dir
+	    rm -rf .git
+        echo "delete .git from folder "$dir
+        cd ..
+    done
+}
+
+cleanup
+cloneall
+delete_dotgit_dirs
