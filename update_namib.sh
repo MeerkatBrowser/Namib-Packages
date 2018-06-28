c3ce81ecae07d6667c24c89cff059bd3998212b7
frederic2ec <freddy.cho@hotmail.com>
2018-06-27 23:33:49 -0400

    Update

--- update_namib.sh
+++ update_namib.sh
@@ -22,7 +22,7 @@ function cleanup() {
 function cloneall() {
     git clone https://github.com/namiblinux/namib-keyring
     git clone https://github.com/namiblinux/namib-mirrorlist
-    git clone https://github.com/MeerkatBrowser/namib-settings-manager
+    git clone https://github.com/namiblinux/namib-settings-manager
     git clone https://github.com/MeerkatBrowser/nhwd
     git clone https://github.com/MeerkatBrowser/nhwd-db
     git clone https://github.com/namiblinux/ckbcomp
